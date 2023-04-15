dd7f073bca2dede5de178038ecf9157a1f186f05
azu <azuciao@gmail.com>
2023-04-15 11:33:25 +0900

    update style

--- src/service.example.js
+++ src/service.example.js
@@ -5,6 +5,11 @@ module.exports = [
         name: "twitter",
         indexPath: path.join(__dirname, "services/twitter/index.js"),
         options: {
+            // 0. Visit https://developer.twitter.com/en/portal/dashboard
+            // 1. Create App with read and write permission
+            // 2. Generate Consumer Key and Secret
+            // 3. Generate Access Token and Secret
+            // 4. Fill the following fields
             // Consumer Keys
             appKey: "app key",
             appSecret: "app secret",
