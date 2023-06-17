4ec7bfe8d251455aa0ad0f93d1ce2baaae73855f
azu <azuciao@gmail.com>
2023-06-17 13:58:36 +0900

    feat(bluesky): support bsky.social
    - bluesky posting support

---
+++ src/services/bluesky/BlueskyClient.js
@@ -0,0 +1,67 @@
+// LICENSE : MIT
+"use strict";
+import { BskyAgent } from "@atproto/api";
+import { truncate } from "tweet-truncator";
+
+export default class BlueskyClient {
+    /**
+     * @param {{ identifier:string; appPassword:string; }} serviceOptions
+     */
+    constructor(serviceOptions) {
+        this.serviceOptions = serviceOptions;
+    }
+
+    get _hasServiceOptions() {
+        return Boolean(this.serviceOptions && this.serviceOptions.identifier && this.serviceOptions.appPassword);
+    }
+
+    isLogin() {
+        return this._hasServiceOptions;
+    }
+
+    loginAsync(callback) {
+        callback(
+            new Error(`Please set Twitter App Key and Secret to options
+    {
+        enabled: true,
+        name: "bluesky",
+        indexPath: path.join(__dirname, "services/bluesky/index.js"),
+        options: {
+            identifier: "you.bsky.social",
+            appPassword: "app password for bsky"
+        }
+    }
+`)
+        );
+    }
+
+    /**
+     *
+     * @param options
+     * @returns {*}
+     * {
+            url,
+            comment,
+            tags = []
+        }
+     */
+    async postLink(options = {}) {
+        const { title, url, comment, tags, quote } = options;
+        // make contents object
+        const contents = { title, url, desc: comment, tags, quote: quote || "" };
+        const status = truncate(contents, {
+            template: `%desc% %quote% "%title%" %url% %tags%`,
+            maxLength: 300
+        });
+        const agent = new BskyAgent({
+            service: "https://bsky.social"
+        });
+        await agent.login({
+            identifier: this.serviceOptions.identifier,
+            password: this.serviceOptions.appPassword
+        });
+        return agent.post({
+            text: status
+        });
+    }
+}
---
+++ src/services/bluesky/BlueskyModel.js
@@ -0,0 +1,10 @@
+// LICENSE : MIT
+"use strict";
+export default class BlueskyModel {
+    constructor() {
+        this.id = "bsky.app";
+        this.name = "BlueskeyModel";
+        this.description = "Bluesky";
+        this.icon = __dirname + "/bluesky.jpg";
+    }
+}
