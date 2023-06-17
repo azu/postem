c038f564686074952ee2a2eb850f04a889cc6100
azu <azuciao@gmail.com>
2023-06-17 14:41:00 +0900

    use debug

--- src/service.browser.js
+++ src/service.browser.js
@@ -1,14 +1,6 @@
 const path = require("path");
 module.exports = [
     {
-        enabled: true,
-        name: "jser.info API",
-        isDefaultChecked: true,
-        index: require("./services/jser.info-api/index")
-    },
-    // Last service is special
-    // Assign shortcut as Cmd + 0
-    {
         enabled: process.env.NODE_ENV === "development",
         name: "debug",
         index: require("./services/debug/index")
